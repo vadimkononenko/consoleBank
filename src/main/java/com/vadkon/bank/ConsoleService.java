79bda1f21523a0474d3e3c1bdc447188aaa9e526
vadimkononenko <vadiskon@gmail.com>
2022-07-17 19:08:49 +0300

    Feature: added impl of menu methods, handle of menu in loop

--- src/main/java/com/vadkon/bank/ConsoleService.java
+++ src/main/java/com/vadkon/bank/ConsoleService.java
@@ -1,8 +1,18 @@
 package com.vadkon.bank;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleService {
+    private double balance;
+    private double creditLimit;
+
+    public ConsoleService() {
+        balance = 0;
+        creditLimit = 1000;
+    }
+
     private String handleInputLogin() {
         ValidatorService validator = new ValidatorService();
         Scanner scanner = new Scanner(System.in);
@@ -27,6 +37,48 @@ public class ConsoleService {
         }
     }
 
+    private void add() {
+        Scanner scanner = new Scanner(System.in);
+        if (!scanner.hasNextDouble()) return;
+
+        double amount = scanner.nextDouble();
+        if (amount > 0)
+            balance += amount;
+        System.out.println("Now your balance: " + balance);
+    }
+
+    private void withdraw() {
+        Scanner scanner = new Scanner(System.in);
+        if (!scanner.hasNextDouble()) return;
+
+        double amount = scanner.nextDouble();
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Now your balance: " + balance);
+        } else {
+            balance = 0;
+            creditLimit -= amount + 10;
+            System.out.println("Your balance: " + balance + ", credit limit left: " + creditLimit);
+        }
+    }
+
+    private void transfer() {
+        Scanner scanner = new Scanner(System.in);
+        if (!scanner.hasNextDouble()) return;
+
+        double amount = scanner.nextDouble();
+        balance -= amount;
+        System.out.println("Balance: " + balance);
+    }
+
+    private void checkBalance() {
+        System.out.println("Balance: " + balance);
+    }
+
+    private void checkCreditLimit() {
+        System.out.println("Credit limit: " + creditLimit);
+    }
+
     private void getDataFromConsole() {
         System.out.print("LOGIN: ");
         handleInputLogin();
@@ -36,8 +88,50 @@ public class ConsoleService {
         handleInputPassword();
     }
 
+    private void handleMenu() {
+        Scanner scanner = new Scanner(System.in);
+        List<String> commandHistory = new ArrayList<>();
+
+        while (!commandHistory.contains("q")) {
+            String input = scanner.nextLine();
+            switch (input) {
+                case "wd":
+                    commandHistory.add(input);
+                    withdraw();
+                    break;
+                case "t":
+                    commandHistory.add(input);
+                    transfer();
+                    break;
+                case "add":
+                    commandHistory.add(input);
+                    add();
+                    break;
+                case "cb":
+                    commandHistory.add(input);
+                    checkBalance();
+                    break;
+                case "ccl":
+                    commandHistory.add(input);
+                    checkCreditLimit();
+                    break;
+                case "q":
+                    commandHistory.add(input);
+                    System.out.println("Have a good day!");
+                    return;
+                default:
+                    commandHistory.add("Error: unavailable value - " + input);
+                    System.out.println("Choose available operation");
+            }
+        }
+    }
+
     public void run() {
         System.out.println("Hello! Please authorise to get access to your account");
         getDataFromConsole();
+        System.out.println("You have been successfully authorised!");
+        System.out.println("Enter to continue: wd - to withdraw, t - to transfer, add - to top up, " +
+                "cb - to check balance, ccl - to check credit limit");
+        handleMenu();
     }
 }
