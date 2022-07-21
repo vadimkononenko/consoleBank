1674f5a81545b3d2c457d6531c1a6207a0f58ac5
vadimkononenko <vadiskon@gmail.com>
2022-07-21 12:41:00 +0300

    Feature: added app.png to screenshots folder

--- src/main/java/com/vadkon/bank/Main.java
+++ src/main/java/com/vadkon/bank/Main.java
@@ -1,14 +1,6 @@
 package com.vadkon.bank;
 
 public class Main {
-
-    //  $ Hello! Please authorise to get access to your account
-    //  $ Login: ...
-    //  $ Password: ...
-    //  $ You have been successfully authorised!
-    //  $ Enter to continue: wd - to withdraw, t - to transfer, add - to top up,
-    //    cb - to check balance, ccl - to check credit limit
-
     public static void main(String[] args) {
         ConsoleService consoleService = new ConsoleService();
         consoleService.run();
